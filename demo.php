06591293ef664b5fb8e2804c446c789e04c24383
bean <dennis2030@gmail.com>
2015-08-21 22:54:35 +0800

    modify AJAX part

--- demo.php
+++ demo.php
@@ -11,44 +11,21 @@
 
         <div class="container">
 
-            <h1>Gridder V2 Demo</h1>
-            <p class="headline">Brought to you by <a href="http://www.oriongunning.com">www.oriongunning.com</a>
+            <p class="headline">
 
 			<?php
-			include 'api/key.php';
-
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, 'https://global.api.pvp.net/api/lol/static-data/oce/v1.2/champion?api_key='.$key);
-
-			// Set so curl_exec returns the result instead of outputting it.
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			// Get the response and close the channel.
-			$response = curl_exec($ch);
-			curl_close($ch);
-
-			$json = json_decode($response, true);
-
-			/*
-			echo'<pre style="color: #FFF;">';
-			print_r($json);
-			echo'</pre>
-			*/
+                $files = glob('./images/*.jpg', GLOB_BRACE);
+                echo count($files);
 			?>
 
 			<ul class="gridder-list">
-				<?php foreach($json['data'] as $key => $champion): ?>
+				<?php foreach($files as $key => $value): ?>
 
-					<?php
-					$str_replace_strings = array(' ',',',"'",'.');
-					$url_image = str_replace($str_replace_strings,'', $champion['name']);
-					?>
-
-					<li class="item item-<?php echo $champion['id']; ?> do-expand-item">
-                        <a href="item-<?php echo $champion['id']; ?>" title="<?php echo $champion['name']; ?> | <?php echo $champion['title']; ?>" class="link">
-                            <div class="image"><img src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/<?php echo $url_image; ?>_0.jpg" /></div>
-                            <span class="title"><?php echo $champion['name']; ?></span>
-                            <span class="description"><?php echo $champion['title']; ?></span>
+                    <li class="item item-<?php echo $key; ?> do-expand-item">
+                        <a rel="nofollow" href="item-<?php echo $key; ?>" title="<?php echo $key; ?>" class="link">
+                            <div class="image"><img src="<?php echo $value?>" /></div>
+                            <span class="title"><?php echo $key; ?></span>
+                            <span class="description"><?php echo $key; ?></span>
                         </a>
 				<?php endforeach; ?>
             </ul>
